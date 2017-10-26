e64600698b1a07339732d3bc88ae8d4709e0d78c
findman <findman@gmail.com>
2017-10-26 21:05:12 +0800

    添加移除空文件夹功能

--- removeBakFile.py
+++ removeBakFile.py
@@ -2,18 +2,18 @@
 # -*- coding:utf-8 -*-
 
 """Usage:
-  removeBakFile.py [-s]
-  removeBakFile.py dir <dir> [<exts>] [-s]
-  removeBakFile.py <exts> [-s]
+  removeBakFile.py [-s] [-e]
+  removeBakFile.py dir <dir> [<exts>] [-s] [-e]
+  removeBakFile.py <exts> [-s] [-e]
 
 
 Options:
   -h --help     Show this screen
   -s            silent remove
+  -e            remove empty folder
   dir           dir, if none use current dir
   exts          bakfile ext. if none default 'bak'. more ext:'bak,bak2,...'
 """
-
 
 import os
 import os.path
@@ -21,27 +21,41 @@ from docopt import docopt
 
 def getFileList(dir):
     fileList = []
+    dirList = []
     for root, dirs, files in os.walk(dir):
+        
+        for dir in dirs:
+            dir = root + '/' + dir
+            dir = dir.replace('\\','/')
+            dirList.append(dir)
+
         for name in files:
             filePath = root + '/' + name
             filePath = filePath.replace('\\','/')
             fileList.append(filePath)
-    return fileList
+
+    return fileList, dirList
 
 def grepList(fileList,exts=['bak']):
     targetList =[]
+
     for filePath in fileList:
         ext = filePath.split('.')[-1]
         if ext in exts:
             targetList.append(filePath)
+
     return targetList
 
-def removeBakFiles(dir,exts=[],silent=False):
+def removeBakFiles(dir, exts=[], silent=False, empty=False):
+    fileList = []
+    dirs = []
+    fileList, dirs = getFileList(dir)
+
     if len(exts):
         exts = exts.split(',')
-        targetList = grepList(getFileList(dir),exts)
+        targetList = grepList(fileList,exts)
     else:
-        targetList = grepList(getFileList(dir))
+        targetList = grepList(fileList)
     
     if not silent:        
         print('==== Remove List ====')
@@ -58,12 +72,35 @@ def removeBakFiles(dir,exts=[],silent=False):
             print('Cancel remove list.')
     else:
         for filePath in targetList:
-            os.remove(filePath)    
+            os.remove(filePath)
+
+    if not silent:     
+        print('==== Remove Empty Folder List ====')
+
+    if empty:        
+        while True:
+            dirs,removeList = removeEmptyFolder(dirs)
+            if (not silent) and removeList:
+                for l in removeList:
+                    print('Remove empty folder: %s' % l)
+            if not removeList: break
+
+def removeEmptyFolder(dirs):
+    removeList = []
+    
+    for dir in dirs:
+        if not os.listdir(dir):
+            os.rmdir(dir)
+            removeList.append(dir)
+            dirs.remove(dir)   
+
+    return dirs,removeList
 
 def main():
     args = docopt(__doc__, version='Remove Bak File')
    
     currentPath = os.getcwd().replace('\\','/') + '/'
+
     kwargs = {}
     dir = ''
     exts = ''
@@ -75,9 +112,9 @@ def main():
         dir = args['<dir>']
     
     if args['<exts>']:
-        kwargs = {'dir':dir, 'exts':args['<exts>'], 'silent':args['-s']}
+        kwargs = {'dir':dir, 'exts':args['<exts>'], 'silent':args['-s'],'empty':args['-e']}
     else:
-        kwargs = {'dir':dir,'silent':args['-s']} 
+        kwargs = {'dir':dir,'silent':args['-s'],'empty':args['-e']} 
         
     removeBakFiles(**kwargs)
 
